7bec5964b6abd106e8e62ac7464619595d89220f
Ryan Nazareth <ryankarlos@gmail.com>
2022-08-09 21:20:16 +0100

    update cookiecutter json and move metadata and setup options to cfg

--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -1,8 +1,16 @@
+
 from setuptools import find_packages, setup
+with open('requirements.txt', 'r') as f:
+    req = [p.strip('\n') for p in f.readlines()]
+
+with open('requirements_dev.txt', 'r') as f:
+    opt = [p.strip('\n') for p in f.readlines()]
 
 setup(
     name='src',
     packages=find_packages(),
+    install_requires=req,
+    extras_require ={'dev': opt},
     version='0.1.0',
     description='{{ cookiecutter.description }}',
     author='{{ cookiecutter.author_name }}',
